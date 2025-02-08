277e1c973aa55a4afe7d339f4f0090e308d74281
vandexy <146407623+vandexy@users.noreply.github.com>
2025-02-08 22:35:36 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -31,7 +31,6 @@ from datetime import datetime as dt
 from logging import DEBUG, INFO, basicConfig, getLogger, warning
 from pathlib import Path
 
-from html_telegraph_poster import TelegraphPoster
 from telethon import Button, TelegramClient, errors, events, functions, types
 from telethon.sessions import StringSession
 from telethon.utils import pack_bot_file_id
