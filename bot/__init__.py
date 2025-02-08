4a66f1345dbbdb66e330aaea19ba9c8ab2190e82
vandexy <146407623+vandexy@users.noreply.github.com>
2025-02-08 22:40:32 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,28 +1,14 @@
-#    This file is part of the Compressor distribution.
-#    Copyright (c) 2021 
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, version 3.
-#
-#    This program is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#    General Public License for more details.
-#
-# License can be found in <
-# https://github.com/> .
-
-
 import asyncio
 import glob
 import inspect
 import io
+import itertools
 import json
 import math
 import os
 import re
 import shutil
+import signal
 import subprocess
 import sys
 import time
@@ -31,6 +17,8 @@ from datetime import datetime as dt
 from logging import DEBUG, INFO, basicConfig, getLogger, warning
 from pathlib import Path
 
+import aiohttp
+import psutil
 from telethon import Button, TelegramClient, errors, events, functions, types
 from telethon.sessions import StringSession
 from telethon.utils import pack_bot_file_id
@@ -45,6 +33,6 @@ try:
     bot = TelegramClient(None, API_ID, API_HASH)
 except Exception as e:
     LOGS.info("Environment vars are missing! Kindly recheck.")
-    LOGS.info("Bot Is Quiting...")
+    LOGS.info("Bot is quiting...")
     LOGS.info(str(e))
     exit()
